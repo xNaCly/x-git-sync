2206301a86f3d2913e25b6931615d36d4e170d5a
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-12-16 21:46:35 +0100

    feat: project structure init

---
+++ sync.go
@@ -0,0 +1,13 @@
+package main
+
+// check if files changed, returns true if one of more files changed
+func checkForFileChanges() bool {
+    return false
+}
+
+// executes `git add -A`
+func addAllFiles(){ }
+
+// executes `git commit`
+func commit(){}
+
---
+++ util.go
@@ -0,0 +1,8 @@
+package main
+
+// generates a new commit message
+func genCommitMsg() string {
+    return "here should be the time"
+}
+
+// parses the config file 
